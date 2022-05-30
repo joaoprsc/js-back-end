7715e38500a729eec404a3e75e509569a4f08bc1
João Príscio <85190319+joaoprsc@users.noreply.github.com>
2022-05-30 18:08:04 -0300

    Delete contador2.js

--- fundamentos/contador2.js
+++
@@ -1,7 +0,0 @@
-function contador2(n){
-    return n * (n+1) / 2;
-}
-var t1 = performance.now();
-contador2(10000000000);
-var t2 = performance.now();
-console.log(`time elapsed: ${(t2-t1) / 1000} seconds.`);
